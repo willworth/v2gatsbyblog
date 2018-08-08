3fa5e94ecaef2d03a5a011188653f1df7db0f439
Will Worth <will.worth@gmail.com>
2018-08-08 22:35:41 +0200

    first

---
+++ src/pages/about.js
@@ -0,0 +1,25 @@
+import React from 'react'
+
+const About = () => (
+  <div>
+    <h1>About</h1>
+    <p>I'm a coder with some professional experience looking for a full time role as a software developer.</p>
+                <p>I've worked with wordpress and PHP, as well as done contract work in quality assurance with a 
+                  python stack using flask and a large scale webscraping project. </p>
+                <p>Most recently, I've been focusing on Javascript- with Node, express, and React. 
+                   My desire to improve these skills led me to choose Gatsby to build this site (though, frankly, react seems to be overkill for a simple blog).  </p>
+                <p> If you have any questions about the contents of this site, or want to talk about collaboration (I'm open to opensource work, too), please do get in touch either 
+                via social networks.  (Facebook is my least favourite here, but I'll make an exception for you...</p>
+                                 
+              <p>  <a href="www.linkedin.com/in/willworth">Linkedin</a></p>
+              <p>  <a href="https://twitter.com/worthcoding">Twitter</a></p>
+              <p>  <a href="https://fb.com/willworth">Facebook</a></p>
+              <p><a href="https://github.com/willworth/">My Github</a></p>
+              <p>  Or email me willworthcoding  at  gmail dot com </p>
+
+              
+                <p>Thanks for your interest.</p>
+  </div>
+)
+
+export default About
